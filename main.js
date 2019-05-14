8530c4765730cf945bfb139fd7633e86e07435a3
psmcca07 <47218280+psmcca07@users.noreply.github.com>
2019-05-14 14:01:27 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,13 @@ alert("Loading main.js!"); //Don't change this line
 player = new Player()
 
 //Create your objects
-let hallway = new Room("dusty hallway", "clouds of dust kick up with every step.");
-let vase = new Item("vase", "made of blue glass, chipped on top. Filled with a dark liquid.")
-let room = new Room("dark room", "It is dark");
+let hallway = new Room("You wake up in an endless hallway. There are doors on each side of the hallway.");
+let pentagram = new Room("You enter the room. The number 9 is written all over the walls. In the center of the room there is a devils trap with ingredients to summon a demon.");
 
-hallway.addItem(vase);
+
 hallway.addItem(room);
+pentagram.addItem(ingredients);
+pentagram.addItem(room);
 
 player.location = hallway;
 player.location.enter();
